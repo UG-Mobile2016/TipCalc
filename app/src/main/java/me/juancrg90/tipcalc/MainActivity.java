1899e9a78e56bc91bcae44078f74c13b4414d252
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-10-06 09:10:51 -0500

    Add Butterknife Binding

--- app/src/main/java/me/juancrg90/tipcalc/MainActivity.java
+++ app/src/main/java/me/juancrg90/tipcalc/MainActivity.java
@@ -1,19 +1,49 @@
 package me.juancrg90.tipcalc;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.input.InputManager;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.btnClear)
+    Button btnClear;
+    @Bind(R.id.txtTip)
+    TextView txtTip;
+
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_CHANGE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -25,15 +55,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(item.getItemId() == R.id.action_about) {
+        if (item.getItemId() == R.id.action_about) {
             about();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.btnSubmit)
+    public void handleSubmit() {
+        hideKeyboard();
+    }
+
+
+    private void hideKeyboard() {
+        InputMethodManager inputManager= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        try {
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (NullPointerException npe) {
+            Log.e(getLocalClassName(), Log.getStackTraceString(npe));
+        }
+    }
+
     private void about() {
-        TipCalcApp app = (TipCalcApp)getApplication();
+        TipCalcApp app = (TipCalcApp) getApplication();
         String strUrl = app.getAboutUrl();
 
         Intent intent = new Intent(Intent.ACTION_VIEW);
